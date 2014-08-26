7bcd608429f1c319361f9a822d1b262e150e9b28
Louis Salin <louis.phil@gmail.com>
2014-08-26 15:30:13 -0500

    maybe monad

--- test.rb
+++ test.rb
@@ -42,6 +42,14 @@ class Integer
 	end
 end
 
+# define the policy in your tests:
+# describe "Integer" do
+#   it "should be a monoid" do
+#     1.mempty.should == 0
+#     1.mappend(2).should == 3
+#   end
+# end
+
 class IntM
 	attr_reader :val
 
@@ -110,13 +118,8 @@ s3 = Subscription.new(u2, ["m1"])
 # Monoid.fold([s1, s2])
 # Monoid.fold([s1, s3]) # fail!
 
+Publication = Value.new(:date_subs)
 class Publication
-	attr_reader :date_subs
-
-	def initialize(date_subs)
-		@date_subs = date_subs
-	end
-
 	def mempty
 		Publication.new({})
 	end
@@ -152,11 +155,162 @@ end
 
 ## quicksort
 
-
-
 def func_quick_sort(list)
 	return list if list.length <= 1
 	h, *t = list
 	quick_sort(t.select { |i| i < h }) + [h] + quick_sort(t.select { |i| i >= h })
+end
+
+## Maybe
+
+# ex without Maybe
+# c = Customer.find(id)
+# if c && c.order
+# 	# do something
+# end
+
+# but what if instead we could:
+# fetch_customer(123).fmap {|c| c.order}.fmap do |o|
+#   # do something with o
+#
+
+
+Customer = Value.new(:order)
+Order = Value.new(:total)
+
+Maybe = Value.new(:value)
+module Repo
+	def self.fetch_customer(id)
+		# fetch the customer
+		if record.present?
+			Maybe.new(record)
+		else
+			Nothing.new
+		end
+	end
+
+	def self.fetch_existing_customer
+		Maybe.new(Customer.new(Order.new(100)))
+	end
+
+	def self.fetch_bad_customer
+		Nothing.new
+	end
+end
+
+class Maybe
+	def nothing?
+		@value == nil
+	end
+
+	def fmap
+		return Nothing.new if nothing?
+		Maybe.new(yield @value)
+	end
+end
+
+class Nothing < Maybe
+	def initialize
+		super(nil)
+	end
+end
+
+Maybe.new(1).fmap {|i| i + 1}
+Nothing.new.fmap {|i| i + 1}
+Repo.fetch_existing_customer().fmap {|c| c.order.total}
+
+# fmap means Maybe is a functor
+
+# now what if to ship an order we need both a customer's address AND an order? fmap can't help us.
+
+# Maybe.pure(->(addr, order) {Shipper.ship(address, order)}).
+#       apply(customer.address).
+#       apply(customer.order)
+
+Customer = Value.new(:order, :address)
+class Customer
+	def order
+		Maybe.new(@order)
+	end
+
+	def address
+		Maybe.new(@address)
+	end
+end
+
+class Shipper
+	def self.ship(address, order)
+		puts "shipped order to #{address}"
+	end
+end
+
+class Maybe
+	# policy
+	# self.pure: takes a value and wraps it into a Maybe
+	# fmap: takes a function that requires at least one parameters and applies the value inside
+	#       the current Maybe to it. It returns a Maybe with the new value inside.
+	# apply: The current Maybe on which apply is called must wrap a function. The parameter must
+	#        be another Maybe value. The function wrapped in the current maybe is applied to the
+	# 	     value of the passed in Maybe value. The whole thing returns a new Maybe with the new
+	#        value.
+	def self.pure(value)
+		Maybe.new(value)
+	end
+
+	# add currying
+	def fmap(&fn)
+		return Nothing.new if nothing?
+		Maybe.new(fn.curry.(@value))
+	end
+
+	def apply(m)
+		return Nothing.new if nothing?
+		return Nothing.new if m.nothing?
+
+		m.fmap {|v| @value.curry.(v)}
+	end
+end
+
+t = Maybe.pure ->(x){x+1}
+t.apply(Maybe.new(1))
+
+cust = Customer.new(nil, nil)
+Maybe.pure(->(address, order) {Shipper.ship(address, order)}).apply(cust.address).apply(cust.order)
+
+cust = Customer.new(Order.new(123), nil)
+Maybe.pure(->(address, order) {Shipper.ship(address, order)}).apply(cust.address).apply(cust.order)
+
+cust = Customer.new(Order.new(123), "home")
+Maybe.pure(->(address, order) {Shipper.ship(address, order)}).apply(cust.address).apply(cust.order)
+
+maybe_customer = Maybe.new(Customer.new(Order.new(123), "home"))
+maybe_customer.fmap do |cust| 
+	Maybe.pure(->(address, order) {Shipper.ship(address, order)}).apply(cust.address).apply(cust.order)
+end
+
+# monads
+class Account
+	def self.charge(amount)
+		puts "charged customer $#{amount}"
+		true
+	end
+end
+
+class Maybe
+	# input: function must take a value and return a monadic value, or computation, in this case just a Maybe
+	def pass(&fn)
+		return Nothing.new if nothing?
+		fn.(@value)
+	end
+end
+
+maybe_customer.pass do |cust|
+	cust.address.pass do |addr|
+		cust.order.pass do |order|
+			puts "about to charge..."
+			discount = order.total > 100 ? 0 : 10
+			Maybe.pure Account.charge(order.total - discount)
+		end
+	end
 end
 
